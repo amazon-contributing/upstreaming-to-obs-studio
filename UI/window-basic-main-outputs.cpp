fishers3/obs

obs-webrtc: Add Simulcast Support

UI/window-basic-main-outputs.cpp
@@ -565,6 +565,10 @@ void SimpleOutput::LoadStreamingPreset_Lossy(const char *encoderId)
 	if (!videoStreaming)
 		throw "Failed to create video streaming encoder (simple output)";
 	obs_encoder_release(videoStreaming);
+
+	if (config_get_bool(main->Config(), "Stream1", "UseSimulcast")) {
+		CreateSimulcastEncoders(encoderId);
+	}
 }
 
 /* mistakes have been made to lead us to this. */
@@ -867,9 +871,14 @@ void SimpleOutput::Update()
 	default:
 		obs_encoder_set_preferred_video_format(videoStreaming,
 						       VIDEO_FORMAT_NV12);
+		for (auto enc : simulcastEncoders)
+			obs_encoder_set_preferred_video_format(
+				enc, VIDEO_FORMAT_NV12);
 	}
 
 	obs_encoder_update(videoStreaming, videoSettings);
+	SimulcastEncodersUpdate(videoSettings, videoBitrate);
+
 	obs_encoder_update(audioStreaming, audioSettings);
 	obs_encoder_update(audioArchive, audioSettings);
 }
@@ -1189,8 +1198,13 @@ FutureHolder<bool> SimpleOutput::SetupStreaming(obs_service_t *service)
 					outputType = type;
 				}
 
-				obs_output_set_video_encoder(streamOutput,
-							     videoStreaming);
+				obs_output_set_video_encoder2(
+					streamOutput, videoStreaming, 0);
+				for (size_t i = 0; i < simulcastEncoders.size();
+				     i++)
+					obs_output_set_video_encoder2(
+						streamOutput,
+						simulcastEncoders[i], i + 1);
 				obs_output_set_audio_encoder(streamOutput,
 							     audioStreaming, 0);
 				obs_output_set_service(streamOutput, service);
@@ -1729,6 +1743,10 @@ AdvancedOutput::AdvancedOutput(OBSBasic *main_) : BasicOutputHandler(main_)
 		      "(advanced output)";
 	obs_encoder_release(videoStreaming);
 
+	if (config_get_bool(main->Config(), "Stream1", "UseSimulcast")) {
+		CreateSimulcastEncoders(streamEncoder);
+	}
+
 	const char *rate_control = obs_data_get_string(
 		useStreamEncoder ? streamEncSettings : recordEncSettings,
 		"rate_control");
@@ -1849,6 +1867,8 @@ void AdvancedOutput::UpdateStreamSettings()
 	}
 
 	obs_encoder_update(videoStreaming, settings);
+	SimulcastEncodersUpdate(settings,
+				obs_data_get_int(settings, "bitrate"));
 }
 
 inline void AdvancedOutput::UpdateRecordingSettings()
@@ -2338,8 +2358,13 @@ FutureHolder<bool> AdvancedOutput::SetupStreaming(obs_service_t *service)
 					outputType = type;
 				}
 
-				obs_output_set_video_encoder(streamOutput,
-							     videoStreaming);
+				obs_output_set_video_encoder2(
+					streamOutput, videoStreaming, 0);
+				for (size_t i = 0; i < simulcastEncoders.size();
+				     i++)
+					obs_output_set_video_encoder2(
+						streamOutput,
+						simulcastEncoders[i], i + 1);
 				obs_output_set_audio_encoder(streamOutput,
 							     streamAudioEnc, 0);
 
@@ -2885,4 +2910,53 @@ BasicOutputHandler *CreateSimpleOutputHandler(OBSBasic *main)
 BasicOutputHandler *CreateAdvancedOutputHandler(OBSBasic *main)
 {
 	return new AdvancedOutput(main);
+}
+
+void BasicOutputHandler::CreateSimulcastEncoders(const char *encoderId)
+{
+	int rescaleFilter =
+		config_get_int(main->Config(), "AdvOut", "RescaleFilter");
+	if (rescaleFilter == OBS_SCALE_DISABLE) {
+		rescaleFilter = OBS_SCALE_BICUBIC;
+	}
+
+	std::string encoder_name = "simulcast_0";
+	for (auto i = 0; i < 2; i++) {
+		uint32_t width = video_output_get_width(obs_get_video()) /
+				 (1.5 + (.5 * i));
+		width -= width % 2;
+
+		uint32_t height = video_output_get_height(obs_get_video()) /
+				  (1.5 + (.5 * i));
+		height -= height % 2;
+
+		encoder_name[encoder_name.size() - 1] = to_string(i).at(0);
+		auto simulcast_encoder = obs_video_encoder_create(
+			encoderId, encoder_name.c_str(), nullptr, nullptr);
+
+		if (simulcast_encoder) {
+			obs_encoder_set_video(simulcast_encoder,
+					      obs_get_video());
+			obs_encoder_set_scaled_size(simulcast_encoder, width,
+						    height);
+			obs_encoder_set_gpu_scale_type(
+				simulcast_encoder,
+				(obs_scale_type)rescaleFilter);
+			simulcastEncoders.push_back(simulcast_encoder);
+			obs_encoder_release(simulcast_encoder);
+		} else {
+			blog(LOG_WARNING,
+			     "Failed to create video streaming simulcast encoders (BasicOutputHandler)");
+		}
+	}
+}
+
+void BasicOutputHandler::SimulcastEncodersUpdate(obs_data_t *videoSettings,
+						 int videoBitrate)
+{
+	for (size_t i = 0; i < simulcastEncoders.size(); i++) {
+		obs_data_set_int(videoSettings, "bitrate",
+				 videoBitrate / (2 * (i + 1)));
+		obs_encoder_update(simulcastEncoders[i], videoSettings);
+	}
 }

UI/window-basic-settings.cpp
@@ -420,6 +420,7 @@ OBSBasicSettings::OBSBasicSettings(QWidget *parent)
 	HookWidget(ui->authUsername,         EDIT_CHANGED,   STREAM1_CHANGED);
 	HookWidget(ui->authPw,               EDIT_CHANGED,   STREAM1_CHANGED);
 	HookWidget(ui->ignoreRecommended,    CHECK_CHANGED,  STREAM1_CHANGED);
+	HookWidget(ui->useSimulcast,         CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->enableMultitrackVideo,      CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->multitrackVideoMaximumAggregateBitrateAuto, CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->multitrackVideoMaximumAggregateBitrate,     SCROLL_CHANGED, STREAM1_CHANGED);

amazon-contributing/upstreaming-to-obs-studio

(cherry picked from commit 6a1907bbec910347548c51d7a807d004f19f1b83)

UI/window-basic-main-outputs.cpp
@@ -588,6 +588,10 @@ void SimpleOutput::LoadStreamingPreset_Lossy(const char *encoderId)
 	if (!videoStreaming)
 		throw "Failed to create video streaming encoder (simple output)";
 	obs_encoder_release(videoStreaming);
+
+	if (config_get_bool(main->Config(), "Stream1", "EnableSimulcast")) {
+		CreateSimulcastEncoders(encoderId);
+	}
 }
 
 /* mistakes have been made to lead us to this. */
@@ -890,9 +894,14 @@ void SimpleOutput::Update()
 	default:
 		obs_encoder_set_preferred_video_format(videoStreaming,
 						       VIDEO_FORMAT_NV12);
+		for (auto enc : simulcastEncoders)
+			obs_encoder_set_preferred_video_format(
+				enc, VIDEO_FORMAT_NV12);
 	}
 
 	obs_encoder_update(videoStreaming, videoSettings);
+	SimulcastEncodersUpdate(videoSettings, videoBitrate);
+
 	obs_encoder_update(audioStreaming, audioSettings);
 	obs_encoder_update(audioArchive, audioSettings);
 }
@@ -1204,6 +1213,9 @@ SimpleOutput::SetupStreaming(obs_service_t *service,
 		}
 
 		obs_output_set_video_encoder(streamOutput, videoStreaming);
+		for (size_t i = 0; i < simulcastEncoders.size(); i++)
+			obs_output_set_video_encoder2(
+				streamOutput, simulcastEncoders[i], i + 1);
 		obs_output_set_audio_encoder(streamOutput, audioStreaming, 0);
 		obs_output_set_service(streamOutput, service);
 		return true;
@@ -1747,6 +1759,10 @@ AdvancedOutput::AdvancedOutput(OBSBasic *main_) : BasicOutputHandler(main_)
 		      "(advanced output)";
 	obs_encoder_release(videoStreaming);
 
+	if (config_get_bool(main->Config(), "Stream1", "EnableSimulcast")) {
+		CreateSimulcastEncoders(streamEncoder);
+	}
+
 	const char *rate_control = obs_data_get_string(
 		useStreamEncoder ? streamEncSettings : recordEncSettings,
 		"rate_control");
@@ -1867,6 +1883,8 @@ void AdvancedOutput::UpdateStreamSettings()
 	}
 
 	obs_encoder_update(videoStreaming, settings);
+	SimulcastEncodersUpdate(settings,
+				obs_data_get_int(settings, "bitrate"));
 }
 
 inline void AdvancedOutput::UpdateRecordingSettings()
@@ -2351,6 +2369,9 @@ AdvancedOutput::SetupStreaming(obs_service_t *service,
 		}
 
 		obs_output_set_video_encoder(streamOutput, videoStreaming);
+		for (size_t i = 0; i < simulcastEncoders.size(); i++)
+			obs_output_set_video_encoder2(
+				streamOutput, simulcastEncoders[i], i + 1);
 		obs_output_set_audio_encoder(streamOutput, streamAudioEnc, 0);
 
 		if (!is_multitrack_output) {
@@ -2903,4 +2924,53 @@ BasicOutputHandler *CreateSimpleOutputHandler(OBSBasic *main)
 BasicOutputHandler *CreateAdvancedOutputHandler(OBSBasic *main)
 {
 	return new AdvancedOutput(main);
+}
+
+void BasicOutputHandler::CreateSimulcastEncoders(const char *encoderId)
+{
+	int rescaleFilter =
+		config_get_int(main->Config(), "AdvOut", "RescaleFilter");
+	if (rescaleFilter == OBS_SCALE_DISABLE) {
+		rescaleFilter = OBS_SCALE_BICUBIC;
+	}
+
+	std::string encoder_name = "simulcast_0";
+	for (auto i = 0; i < 2; i++) {
+		uint32_t width = video_output_get_width(obs_get_video()) /
+				 (1.5 + (.5 * i));
+		width -= width % 2;
+
+		uint32_t height = video_output_get_height(obs_get_video()) /
+				  (1.5 + (.5 * i));
+		height -= height % 2;
+
+		encoder_name[encoder_name.size() - 1] = to_string(i).at(0);
+		auto simulcast_encoder = obs_video_encoder_create(
+			encoderId, encoder_name.c_str(), nullptr, nullptr);
+
+		if (simulcast_encoder) {
+			obs_encoder_set_video(simulcast_encoder,
+					      obs_get_video());
+			obs_encoder_set_scaled_size(simulcast_encoder, width,
+						    height);
+			obs_encoder_set_gpu_scale_type(
+				simulcast_encoder,
+				(obs_scale_type)rescaleFilter);
+			simulcastEncoders.push_back(simulcast_encoder);
+			obs_encoder_release(simulcast_encoder);
+		} else {
+			blog(LOG_WARNING,
+			     "Failed to create video streaming simulcast encoders (BasicOutputHandler)");
+		}
+	}
+}
+
+void BasicOutputHandler::SimulcastEncodersUpdate(obs_data_t *videoSettings,
+						 int videoBitrate)
+{
+	for (size_t i = 0; i < simulcastEncoders.size(); i++) {
+		obs_data_set_int(videoSettings, "bitrate",
+				 videoBitrate / (2 * (i + 1)));
+		obs_encoder_update(simulcastEncoders[i], videoSettings);
+	}
 }

UI/window-basic-settings.cpp
@@ -420,6 +420,7 @@ OBSBasicSettings::OBSBasicSettings(QWidget *parent)
 	HookWidget(ui->authUsername,         EDIT_CHANGED,   STREAM1_CHANGED);
 	HookWidget(ui->authPw,               EDIT_CHANGED,   STREAM1_CHANGED);
 	HookWidget(ui->ignoreRecommended,    CHECK_CHANGED,  STREAM1_CHANGED);
+	HookWidget(ui->enableSimulcast,      CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->enableMultitrackVideo,      CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->multitrackVideoMaximumAggregateBitrateAuto, CHECK_CHANGED,  STREAM1_CHANGED);
 	HookWidget(ui->multitrackVideoMaximumAggregateBitrate,     SCROLL_CHANGED, STREAM1_CHANGED);